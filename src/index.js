3c024aeb47678db13b9900387ccfe70e3d6d148c
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-10-18 03:16:14 +0800

    Testing the code to saving data to local storage

--- src/index.js
+++ src/index.js
@@ -2,3 +2,20 @@
 import "./style.css";
 
 // console.log("Compilation works, hello world!");
+
+// const userObj = {
+//   username: "Maria",
+//   email: "maria@mail.com",
+// };
+//
+// localStorage.setItem("user", JSON.stringify(userObj));
+
+const storedUserData = localStorage.getItem("user");
+
+if (storedUserData) {
+  const userData = JSON.parse(storedUserData);
+  console.log(userData);
+  // You can use userData here...
+} else {
+  console.log("User data not found in local storage");
+}
