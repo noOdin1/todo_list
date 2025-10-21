a8d3d430fa5679e90f9dd5926d479273f7325c21
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-10-21 17:14:09 +0800

    Function to display all the projects created

--- src/console.js
+++ src/console.js
@@ -2,3 +2,14 @@ const consolePromptInput = (promptStr) => {
   return prompt(promptStr);
 };
 
+
+
+const consoleDisplayProjects = (projectList) => {
+  projectList.forEach((proj) => {
+    console.log(`Project details: 
+        Project id: ${proj.projectId}
+        Project name: ${proj.projectName}
+      `);
+  });
+};
+
