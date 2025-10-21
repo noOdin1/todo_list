f324d9215e1223aa456ee90d32eb5b39021a1e48
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-10-21 17:14:48 +0800

    Function to display all the task created and it's details

--- src/console.js
+++ src/console.js
@@ -13,3 +13,16 @@ const consoleDisplayProjects = (projectList) => {
   });
 };
 
+const consoleDisplayTasks = (taskList) => {
+  taskList.forEach((task) => {
+    console.log(`Task Details: 
+      Task id: ${task.taskId}
+      Task project id: ${task.projectId}
+      Task name: ${task.taskName}
+      Task description/notes: ${task.taskDesc}
+      Task priority: ${task.taskPriority}
+      Task due date: ${task.taskDueDate}
+      `);
+  });
+};
+
