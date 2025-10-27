2b6efa1b571a942b60f7753ff1d517dec636302b
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-10-28 04:24:34 +0800

    Removed old example code on how to save info to localstorage

--- src/index.js
+++ src/index.js
@@ -16,23 +16,6 @@ import {
 import { addFormEventListener } from "./webui.js";
 import { isFuture } from "date-fns";
 
-// console.log("Compilation works, hello world!");
-
-// const userObj = {
-//   username: "Maria",
-//   email: "maria@mail.com",
-// };
-//
-// localStorage.setItem("user", JSON.stringify(userObj));
-
-// const storedUserData = localStorage.getItem("user");
-//
-// if (storedUserData) {
-//   const userData = JSON.parse(storedUserData);
-//   console.log(userData);
-//   // You can use userData here...
-// } else {
-//   console.log("User data not found in local storage");
 // }
 
 const todoApp = (function () {
