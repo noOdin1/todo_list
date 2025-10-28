53cdb28690db339aa2c20be0fa972615e1424789
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-10-28 16:19:10 +0800

    Import tools for this module

--- src/webui.js
+++ src/webui.js
@@ -4,3 +4,17 @@ export { addFormEventListener };
 import { v4 as uuidv4 } from "uuid";
 import { Task, Project } from "./index.js";
 import { projectList, taskList } from "./index.js";
+import {
+  elementFactory,
+  createP,
+  createDiv,
+  createButton,
+  createImg,
+  createForm,
+  createFieldset,
+  createLegend,
+  createLabel,
+  createInput,
+  createSelect,
+  createOption,
+} from "./element_creator.js";
